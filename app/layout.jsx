d706bf5f3051a412dcd5ab26aba598c94d3604ff
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-25 13:06:00 +0530

    some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ const RootLayout = ({children}) => {
       <body>
         <Provider>
         <div className='main'>
-          < div className='graient' />
+          < div className='gradient' />
         </div>
 
         <main className='app'>
